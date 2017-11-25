84d1923f8706f7e88673cc067344971752a4c1a9
Neous1 <Neous1@users.noreply.github.com>
2017-11-25 12:39:56 -0500

    create a object of person
    create get and set method in person to access birthdate fied

--- AccessModifiers/AccessModifiers/Program.cs
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -9,12 +9,26 @@ namespace AccessModifiers
     public class Person
     {
         private DateTime _birthdate;
+
+        //create method to access birthdate
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
     }
 
 class Program
     {
         static void Main(string[] args)
         {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1980,3,31));
+            Console.WriteLine(person.GetBirthdate());
         }
     }
 }
