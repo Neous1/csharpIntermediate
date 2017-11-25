558bf331433f76f66e20584ca2a9f31305e3e3e8
Neous1 <Neous1@users.noreply.github.com>
2017-11-25 10:48:10 -0500

    create customer class

---
+++ Fields/Fields/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
+
+class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
