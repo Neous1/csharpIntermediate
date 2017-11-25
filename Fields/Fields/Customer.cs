7d8c565e0ca454fa13095b4c39ad33b55db8798f
Neous1 <Neous1@users.noreply.github.com>
2017-11-25 10:57:18 -0500

    separarated the classes for readibility

---
+++ Fields/Fields/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders = new List<Order>();
+
+        // create constructors for customer
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id,string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            
+        }
+    }
+}
--- Fields/Fields/Program.cs
+++ Fields/Fields/Program.cs
@@ -1,36 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fields
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-        public List<Order> Orders = new List<Order>();
-
-        // create constructors for customer
-        public Customer(int id)
-        {
-            this.Id = id;
-        }
-
-        public Customer(int id,string name)
-            :this(id)
-        {
-            this.Name = name;
-        }
-    }
-
-    public class Order
-    {
-        
-    }
-
-class Program
+    class Program
     {
 
         static void Main(string[] args)
