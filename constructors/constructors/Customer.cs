9e3b11d53ab2b567b673397646ff090c1af933ab
Neous1 <Neous1@users.noreply.github.com>
2017-11-25 06:24:41 -0500

    using "this" keyword to pass execution to anther constructor

--- constructors/constructors/Customer.cs
+++ constructors/constructors/Customer.cs
@@ -1,22 +1,28 @@
-﻿namespace constructors
+﻿using System.Collections.Generic;
+
+namespace constructors
 {
     public class Customer
     {
         public int Id;
         public string Name; // keyworkd public used here to illustrate. 
+        public List<Order> Orders;
 
+        //default parameterless constructor
         public Customer()
         {
+            Orders = new List<Order>();
         }
 
         public Customer(int id)
+            :this() // this is meant to call the 1st overload
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
+            : this(id) // this is meant to call the 2nd overload which in turn calls the 1st overload
         {
-            this.Id = id;
             this.Name = name;
         }
     }
