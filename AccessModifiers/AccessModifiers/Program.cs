95e264dd3addf6dc4ba5977b3ddaa689655d1cb3
Neous1 <Neous1@users.noreply.github.com>
2017-11-25 12:31:53 -0500

    create a Person class with a private birthdate field inaccessible outside the class

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+    }
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
