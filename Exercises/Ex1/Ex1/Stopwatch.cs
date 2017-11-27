8e8513b6a9749b2a87a94a0649c6906b0fd36836
Neous1 <Neous1@users.noreply.github.com>
2017-11-27 10:27:48 -0500

    set up clocked property for get and set

---
+++ Exercises/Ex1/Ex1/Stopwatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex1
+{
+    public class Stopwatch
+    {
+        public DateTime Clocked { get; set; }
+
+        public int Start()
+        {
+
+            return 0;
+        }
+
+        public int Stop()
+        {
+
+            return 0;
+        }
+    }
+
+
+}
