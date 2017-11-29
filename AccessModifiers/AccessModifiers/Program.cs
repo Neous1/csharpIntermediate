5d16ece05f4bcde60db6cda26dc7b133e4bd89f1
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 14:19:47 -0500

    create a promote and calculaterating method with promote calling calculatingRating()

--- AccessModifiers/AccessModifiers/Program.cs
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -27,7 +27,19 @@ namespace AccessModifiers
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public void Promote()
+        {
+            var rating = CalculateRating();
+            if (rating == 0)
+                Console.WriteLine("Promoted to level 1");
+            else
+                Console.WriteLine("Promoted to level 2");
+        }
 
+        public int CalculateRating()
+        {
+            return 0;
+        }
     }
 
 class Program
