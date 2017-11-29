9e339c9172a15b73951aff09af4b16ff6cb283a8
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 16:42:42 -0500

    create a vehicle class create a car class which derives from vehicle

---
+++ Section 4/Constructors/Constructors/Car.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Constructors
+{
+    //define a class Car that derives from vehicle
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("car is being initialized");
+        }
+    }
+}
---
+++ Section 4/Constructors/Constructors/Vehicle.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Constructors
+{
+    public class Vehicle
+    {
+        // create a default constructor
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized");
+        }
+
+
+    }
+    
+}
