dc8fa113711c841ecb1a948eb0a2da1b1167e34b
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 13:23:49 -0500

    create a dbMigrator object adn pass it a logger object

--- Composition/Composition/Program.cs
+++ Composition/Composition/Program.cs
@@ -5,25 +5,12 @@ using System.Threading.Tasks;
 
 namespace Composition
 {
-    public class Installer
-    {
-        private readonly Logger _logger;
-
-        public Installer(Logger logger)
-        {
-            _logger = logger;
-        }
-
-        public void Install()
-        {
-            _logger.Log("we are installing the application");
-        }
-    }
     class Program
     {   
 
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new Logger());
         }
     }
 }
