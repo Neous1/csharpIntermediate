eb262a0125d0a728854de1e3c1c55b03469874a0
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 13:15:16 -0500

    create a DbMigrator class and a private constructor

--- Composition/Composition/Program.cs
+++ Composition/Composition/Program.cs
@@ -5,6 +5,19 @@ using System.Threading.Tasks;
 
 namespace Composition
 {
+    public class DbMigrator
+        //class responsible for migrating the database
+    {
+        private readonly Logger _logger;
+
+        //create a constructor with a parameter of type Logger to create 
+        // an association to the logger class
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+    }
     class Program
     {   
 
