c0da276e55a05d808c53f3860fd3eeb15a81cbed
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 12:09:59 -0500

    create a class text which derives from presentationObject

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,10 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Inheritance
 {
+    //create a class that derives from presentationObject
+
+    public class Text: PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
