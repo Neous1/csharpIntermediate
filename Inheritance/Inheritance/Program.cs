0b8ef6a07fbe1f259f2c418c5c419effc7634a57
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 12:03:30 -0500

    create a method copy

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -10,6 +10,14 @@ namespace Inheritance
     {
         public int Width { get; set; }
         public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard");
+
+        }
+
+        
     }
     class Program
     {
