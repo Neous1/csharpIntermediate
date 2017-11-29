c9cf2a0174486750c3ddd5e4739e0136775ddb1f
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 12:58:41 -0500

    create logger class with a log method which takes a message as a param and prints it tothe console

---
+++ Composition/Composition/Program.Logger.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Composition
+{
+    partial class Program
+    {
+        public class Logger
+        {
+            public void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
