37b10f3c103bcc002701a46e8ca455174ba5a79f
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 13:18:39 -0500

    create a class Installer and a readonly constructor

--- Composition/Composition/Program.cs
+++ Composition/Composition/Program.cs
@@ -5,6 +5,15 @@ using System.Threading.Tasks;
 
 namespace Composition
 {
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+    }
     class Program
     {   
 
