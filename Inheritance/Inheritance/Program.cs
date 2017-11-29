687d1e9820ca178c26b137f3df1c0fd790697b61
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 12:10:27 -0500

    separate class Text from Main program

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +7,6 @@ namespace Inheritance
 {
     //create a class that derives from presentationObject
 
-    public class Text: PresentationObject
-    {
-        public int FontSize { get; set; }
-        public string FontName { get; set; }
-
-        public void AddHyperlink(string url)
-        {
-            Console.WriteLine("We added a link to " + url);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
