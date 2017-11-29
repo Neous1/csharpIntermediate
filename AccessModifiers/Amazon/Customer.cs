6937f9c932ce6cef81e7d330e706f0f58d190bf3
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 14:47:27 -0500

    create a class library Amazon for our project

---
+++ AccessModifiers/Amazon/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            Console.WriteLine("promote logic change");
+        }
+
+
+
+    }
+}
