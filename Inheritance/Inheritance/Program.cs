b8260a1af33d3444513f5a50a40e994e8b2c0bbc
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 12:14:09 -0500

    create an object for text text will call copy() which it inherited from presentationObjec

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -11,6 +11,9 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
         }
     }
 }
