16a9274663c3599299cf66031b0ab561d45e27b7
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 13:17:29 -0500

    create a migrate method and move dbMigrate toa separe file

---
+++ Composition/Composition/DbMigrator.cs
@@ -0,0 +1,21 @@
+﻿namespace Composition
+{
+    public class DbMigrator
+        //class responsible for migrating the database
+    {
+        private readonly Logger _logger;
+
+        //create a constructor with a parameter of type Logger to create 
+        // an association to the logger class
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We are migrating blah blah .....");
+        }
+
+    }
+}
