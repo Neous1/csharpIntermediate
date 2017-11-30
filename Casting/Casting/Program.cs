847e64c998fad0bd32fab8273cabfa3a684e26c7
Neous1 <Neous1@users.noreply.github.com>
2017-11-29 19:30:22 -0500

    cast sender to button in order to gain its attribute

--- Casting/Casting/Program.cs
+++ Casting/Casting/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +12,28 @@ namespace Casting
     {
         static void Main(string[] args)
         {
-            Text text = new Text();
-            Shape shape = text;
+//            Text text = new Text();
+//            Shape shape = text;
+//
+//            text.Width = 200;
+//            shape.Width = 100;
+//
+//            Console.WriteLine(text.Width);// 100 because they both point to the same object
 
-            text.Width = 200;
-            shape.Width = 100;
 
-            Console.WriteLine(text.Width);// 100 because they both point to the same object
+            //StreamReader reader = new StreamReader(new MemoryStream());
 
+            /*var list = new ArrayList();
+            list.Add(1);
+            list.Add("Yvon");
+            list.Add(new Text());*/
+
+            //var list = new List<Shape>();// shape object is passed here.
+
+
+//            Shape shape = new Text();
+//            Text text = (Text) shape; //cast shape to text gives it access to more attibure
+//            
 
         }
     }
