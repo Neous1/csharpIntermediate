e9df3619d7239feaab641693ff9849dca3db8ff5
Neous1 <Neous1@users.noreply.github.com>
2017-11-30 09:28:21 -0500

    inheritance exercise

---
+++ Exercises/Section4/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Stack
+    {
+        public void Push(object obj)
+        {
+            
+        }
+
+        public object Pop()
+        {
+            return 0;
+        }
+
+        private void Clear()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            foreach (object obj in stack)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
