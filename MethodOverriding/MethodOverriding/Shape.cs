ac9c2dbb713f0a86d3b66eb799b7e52608aba40a
Neous1 <Neous1@users.noreply.github.com>
2017-12-04 19:22:26 -0500

    remove the enumaration ShapeType as it wil lbe handled by but the sub classes

--- MethodOverriding/MethodOverriding/Shape.cs
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -14,6 +14,5 @@
         public int Width { get; set; }
         public int Height { get; set; }
         public Position Position { get; set; }
-        public ShapeType Type { get; set; }
     }
 }
