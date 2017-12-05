abe1ca5bc35baeba9ac8ee6412d7a139313266ef
Neous1 <Neous1@users.noreply.github.com>
2017-12-04 19:13:17 -0500

    create a class Shpe with various properties

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+
+        public class Shape
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public Position Position { get; set; }
+            public ShapeType Type { get; set; }
+        }
+        static void Main(string[] args)
+        { 
+        }
+    }
+}
