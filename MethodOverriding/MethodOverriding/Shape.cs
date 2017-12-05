1ce0d6235e526806e6a7f8f920d2fa657f8e26ca
Neous1 <Neous1@users.noreply.github.com>
2017-12-04 19:59:49 -0500

    Add triangle class derived from shape

--- MethodOverriding/MethodOverriding/Shape.cs
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -19,11 +19,20 @@ namespace MethodOverriding
 
         }
     }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+
+        }
+    }
     public class Shape
     {
         public int Width { get; set; }
         public int Height { get; set; }
-        public Position Position { get; set; }
+        //public Position Position { get; set; }
 
         //virtual keyworld makes draw() overidable in other subclass
         // that need to draw as a base method
