1d1293c7a327541aa01fc764c179d38448753ddf
Neous1 <Neous1@users.noreply.github.com>
2017-12-04 19:36:44 -0500

    create a class Canvas in which we have a list which iterate thru the type of shapes and draws them as they are call

--- MethodOverriding/MethodOverriding/Program.cs
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace MethodOverriding
 {
+    public class Canvas
+    {
+        public void Drawshapes(List<Shape> shapes)
+        {
+            // polymorphism
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
     class Program
     {
-
-
         static void Main(string[] args)
         { 
         }
