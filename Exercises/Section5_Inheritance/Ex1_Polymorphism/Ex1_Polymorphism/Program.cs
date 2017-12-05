893cfcb4f1d57122beca859a104aaa2f26a90c75
Neous1 <Neous1@users.noreply.github.com>
2017-12-05 18:33:50 -0500

    create dbConnection class

---
+++ Exercises/Section5_Inheritance/Ex1_Polymorphism/Ex1_Polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_Polymorphism
+{
+    class DbConnection
+    {
+        private string _connectionString;
+
+        public string ConnectionString
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_connectionString))
+                {
+                    Console.WriteLine("valid connection string is needed to connect");
+                }
+                return _connectionString;
+            }
+            set { _connectionString = value; }
+        }
+
+        public TimeSpan Timeout { get; set; }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
