b24cd538ef7b40ec7a5277e72e8d2b8e4865492a
Neous1 <Neous1@users.noreply.github.com>
2017-12-07 13:54:15 -0500

    create class dbconnection create properties connections string and timeout create method openconnection adn close connection

---
+++ Exercises/Section6_Polymorphism/Ex_1_DatabaseConnection/Ex_1_DatabaseConnection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1_DatabaseConnection
+{
+    public class DbConnection
+    {
+        private string _connectionString;
+
+        /*ConnectionString : string
+Timeout : TimeSpan*/
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+            set
+            {
+                if (_connectionString == null)
+                    Console.WriteLine("connection string needed to connect");
+                _connectionString = value;
+            }
+        }
+
+        public TimeSpan Timeout { get; set; }
+
+
+        public DbConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public abstract OpenConnection()
+        {
+            
+        }
+
+        public as CloseConnection()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
